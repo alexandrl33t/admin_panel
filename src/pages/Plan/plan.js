71880c44f71497d7ce378ec81ab412a43e698b57
aleksandr <alexandrl33t@yandex.ru>
2022-10-27 16:11:37 +0300

    план черновик

--- src/pages/Plan/plan.js
+++ src/pages/Plan/plan.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {ButtonStyled} from "../../styledAntd";
-import {Layer, Stage, Image, Group} from "react-konva";
-import {VideoCameraOutlined} from "@ant-design/icons";
+import {Layer, Stage, Image} from "react-konva";
+import camera from "../../static/img/icons/camera.png"
+import { useCropper} from "./hooks/useCropper";
+import useImage from "use-image"
+import { Portal } from 'react-konva-utils';
 
+/**
+ * Возможно нужно переделать с React Drag & Drop:
+ * https://tproger.ru/translations/dran-n-drop-with-react/
+ * https://habr.com/ru/company/kts/blog/647241/#2
+ * */
 class PlanImage extends React.Component {
-
     state = {
         image: null
     };
@@ -24,19 +31,70 @@ class PlanImage extends React.Component {
         return <Image image={this.state.image} />;
     }
 }
-class CameraImage extends React.Component {
-    render() {
-        return <VideoCameraOutlined />
-    }
-}
 
 const Plan = (props) => {
-    const {newObject} = props
 
-    let camerasArray = []
-    for (let i = 1; i <= 10; i++) {
-        camerasArray.push()
+    const {newObject} = props
+    const [image] = useImage(camera, "NONE");
+
+    const USER_IMAGE_LAYER = {
+        width: 50,
+        height: 50
+        // width: 1162,
+        // height: 1155,
+    };
+
+    const MASK_LAYER = {
+        width: 624,
+        height: 591,
+    };
+    let {
+        zoom,
+        x,
+        y,
+        imageRef,
+    } = useCropper({
+        image: image,
+        layer: USER_IMAGE_LAYER,
+        maskLayer: MASK_LAYER
+    });
+
+    const deleteObject = () =>{
+        image.src=""
     }
+
+
+    let devices = [
+    {
+        id: <Image
+            ref={imageRef}
+            image={image}
+            x={x}
+            y={y}
+            draggable
+            scaleX={zoom}
+            scaleY={zoom}
+            onDblClick={deleteObject}
+        />,
+    },
+    {
+        id:  <Image
+            ref={imageRef}
+            image={image}
+            x={x}
+            y={y}
+            draggable
+            scaleX={zoom}
+            scaleY={zoom}
+            onDblClick={deleteObject}
+        />,
+    },
+]
+
+    const addDevice = () => {
+
+    }
+
     const src = "https://images.edrawmax.com/examples/apartment-floor-plan/example4.png"
     const styleButton = {
         alignContent:"center",
@@ -59,19 +117,16 @@ const Plan = (props) => {
                                 <ButtonStyled>Редактировать план</ButtonStyled>
                                 <Stage width={1000} height={800}>
                                     <Layer>
+                                        <Portal enabled={true} selector=".top-layer">
+                                            {devices.map((device) =>
+                                                device.id
+                                            )}
+                                        </Portal>
                                         <PlanImage src={src} />
                                     </Layer>
-                                    <Layer>
-                                        <Group draggable>
-                                            {
-                                                camerasArray.map((item) =>
-                                                    <CameraImage /> //доделать прорисовку камер
-                                                )
-                                            }
-                                        </Group>
-                                    </Layer>
-
+                                        <Layer name="top-layer" />
                                 </Stage>
+                                <ButtonStyled onClick={addDevice}>Добавить устройство</ButtonStyled>
                             </div>
                 )
 
