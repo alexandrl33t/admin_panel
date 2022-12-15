f29a6b377458c842ac5e1a6ea62d92671b11d7f5
aleksandr <alexandrl33t@yandex.ru>
2022-12-15 17:15:51 +0300

    device

--- src/tools/AbstractDevice.js
+++ src/tools/AbstractDevice.js
@@ -1,9 +1,15 @@
-export default class Tool{
+class AbstractDevice{
+
+    id = null
+
+
+
     constructor(canvas) {
         this.canvas = canvas
         this.ctx = canvas.getContext('2d')
-        this.destroyEvents()
     }
+
+
 
     destroyEvents() {
         this.canvas.onmouseup = null
