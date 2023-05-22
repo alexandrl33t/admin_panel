07f227af18c3a7a505b2247b3ac837909656e85e
Alexandr <alexandrl33t@yandex.ru>
2023-05-22 22:17:21 +0300

    пофиксить попап

--- src/components/Map/MapComponent.js
+++ src/components/Map/MapComponent.js
@@ -1,42 +1,60 @@
 
-import { useState } from "react";
+import {useRef, useState} from "react";
 import MyMap from "./MyMap";
 import axios from "axios";
-import {Input} from "antd";
+import {Input, Select} from "antd";
 
 const MapComponent = () => {
-    const [coords, setCorrds] = useState({
+    const [options, setOptions] = useState([])
+    const [coords, setCoords] = useState({
         latitude: "55.74993",
         longitude: "37.61691"
     });
 
 
-    function search(input, e) {
+    const onSearch = (input) => {
         const url = `https://api.maptiler.com/geocoding/${input}.json?key=gEzEVg0cbrHwyqDHzbit`
         axios.get(url)
             .then(response => {
-                console.log(response.data.features)
+                let opt= []
+                response.data.features.forEach(item => {
+                    opt.push({value: item.place_name_ru, label: item.place_name_ru, center: item.center})
+                })
+                setOptions(opt)
             })
             .catch(error => {
                 console.error(error);
             });
     }
-
+    const onSelect = (value, option) => {
+        setCoords({
+            latitude: option.center[1],
+            longitude: option.center[0]
+        })
+    }
 
     return (
         <div>
             <h2>Введите ваш адрес</h2>
             <section>
                 <h2>Поиск адреса</h2>
-                <Input.Search
-                    placeholder="Введите адрес"
+                <Select
+                    showSearch
+                    placeholder="Select a person"
+                    optionFilterProp="children"
+                    onSearch={onSearch}
+                    filterOption={(input, option) =>
+                        (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+                    }
+                    onSelect={onSelect}
                     style={{
                         width: '600px',
                     }}
-                    onSearch={(value, e) => {search(value)}}
+                    options={options}
+                    autoClearSearchValue={false}
                 />
             </section>
-            <MyMap coords={coords} />
+            <MyMap coords={coords} setCoords={setCoords} />
         </div>
     );
 };
--- src/components/Map/MyMap.js
+++ src/components/Map/MyMap.js
@@ -1,23 +1,83 @@
-
-import Map, {Marker, NavigationControl} from 'react-map-gl';
+import ReactMapGL, {Marker, NavigationControl, Popup} from 'react-map-gl';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import '../../App.css';
-export default function MyMap({ coords }) {
+import {useEffect, useRef, useState} from "react";
+import axios from "axios";
+export default function MyMap(props) {
+    const {coords, setCoords } = props
     const { latitude, longitude } = coords;
+    const mapRef = useRef(null)
+    const [viewport, setViewport] = useState({
+        latitude: latitude,
+        longitude: longitude,
+        zoom: 10
+    });
+    const [showPopup, setShowPopup] = useState(false);
+    const [adress, setAdress] = useState('')
+    const [markerClicked, setMarkerClicked] = useState(false)
+
+
+    useEffect(()=>{
+        mapRef.current?.flyTo({center: [longitude, latitude]});
+    }, [longitude, latitude])
+
+    const changeState = (e) => {
+        setViewport(e.viewState)
+    };
+
+    const handleClick = (e) => {
+        setCoords({
+            latitude: e.lngLat.lat,
+            longitude: e.lngLat.lng,
+        })
+    }
+    const markerClickHandler = (e) => {
+        if (!showPopup) {
+            const url = `https://api.maptiler.com/geocoding/${longitude},${latitude}.json?key=gEzEVg0cbrHwyqDHzbit`
+            axios.get(url)
+                .then(response => {
+                    let add = ''
+                    response.data.features.forEach(item => {
+                        add += item.text_ru + ', '
+                    })
+                    setAdress(add)
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+            setShowPopup(true)
+        } else {
+            setShowPopup(false)
+        }
+
+    }
 
     return (
-            <Map mapLib={maplibregl}
-                   initialViewState={{
-                     longitude: longitude,
-                     latitude: latitude,
-                     zoom: 14
-                 }}
-                   style={{width: "600px", height: "400px"}}
-                   mapStyle="https://api.maptiler.com/maps/fa3455ef-48d0-4b05-a0ba-d8f516811220/style.json?key=gEzEVg0cbrHwyqDHzbit"
+            <ReactMapGL
+                {...viewport}
+                ref={mapRef}
+                mapLib={maplibregl}
+               initialViewState={
+                    {...viewport}
+                 }
+                onZoom={changeState}
+                onRotate={changeState}
+                onDrag={changeState}
+                onClick={handleClick}
+                 style={{width: "600px", height: "400px"}}
+                 mapStyle="https://api.maptiler.com/maps/fa3455ef-48d0-4b05-a0ba-d8f516811220/style.json?key=gEzEVg0cbrHwyqDHzbit"
             >
-                <NavigationControl position="top-left" />
-                <Marker longitude={longitude} latitude={latitude}/>
-            </Map>
+                {showPopup && (
+                    <Popup longitude={longitude} latitude={latitude}
+                           anchor="bottom"
+                           onClose={markerClickHandler}
+                           closeOnClick={false}
+                    >
+                        {adress}
+                    </Popup>)}
+                <NavigationControl position="top-left" showCompass={false}/>
+                <Marker longitude={longitude} latitude={latitude} draggable={true} pitchAlignment={'viewport'} onClick={markerClickHandler}/>
+            </ReactMapGL>
     );
 }
